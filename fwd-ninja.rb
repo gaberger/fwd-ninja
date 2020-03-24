29f354555ae47d387a41fc87a69cd7c1c6c1c471
Gary <garyberger@forwardnetworks.com>
2020-03-24 12:22:11 -0400

    update
    Signed-off-by: Gary <garyberger@forwardnetworks.com>

--- fwd-ninja.rb
+++
@@ -1,5 +0,0 @@
-class FwdNinja < Formula
-  homepage "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja"
-  url "https://gerrit.local.forwardnetworks.com/plugins/gitiles/ninja-tap/+/refs/heads/master/ninja.zip , :using => :git"
-  sha256 "60FD9539319D9A3730C5B3C80BA138CDA8B9FAD302AE0BC3B812BEF2280F8E03"
-end
